18b2fa6452543c1fdebe8d1621721850c8e37157
Erik Nuber <erik.nuber@yahoo.com>
2024-06-25 12:47:12 -0700

    variables and operators

---
+++ 1_essentials/2_variabletypes_operators.py
@@ -0,0 +1,42 @@
+# variable types
+# Intergers - whole numbers no quotes used
+# Floats - decimal point numbers no quotes used
+# Strings - any word or text enclosed by quotes
+# Boolean - True / False
+
+# check variable type
+name = 'Erik'
+age = 50
+print(type(name))  # string
+print(type(age))  # int
+
+height = 1.83
+height_int = int(height)
+print(height)  # 1.83
+print(height_int)  # 1
+
+# transform int to float
+a = 1
+b = float(a)
+
+print(b)  # 1.0
+
+# transform float/int to string
+
+c = 3.245
+d = str(c)
+print(d)  # '3.245'
+
+
+# Operators
+
+# basic operators
+# + addition
+# - subtraction
+# * multiplication
+#  / division
+# // floor division
+# % modulus
+# ** exponentiation
+
+#  +=, -=, *=, /=, %=, **=, //=
